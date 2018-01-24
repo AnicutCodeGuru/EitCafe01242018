2d8d129225ed046099956d6c34cd36ff23c4d9b9
Learn <happy.anicut@gmail.com>
2018-01-24 22:16:28 +0530

    Variables

---
+++ 01Variables.1.js
@@ -0,0 +1,27 @@
+
+fname="xyz"
+console.log(fname);
+var fname="Raju";  // Variable hoisting.
+console.log(fname);
+/**
+ * In javascript a variable can be used before we declare it. This concept is called as Variable hoisting.
+ */
+
+
+
+ //String
+
+ var javascript = "Welcome to javascript programming";
+ console.log(javascript.toUpperCase());  // COnverts all letters in the string to uppercase;
+ console.log(javascript.toLowerCase());  // COnverts all letters in the string to lowercase;
+ console.log(javascript.split(" "));  // COnverts all letters in the string to lowercase;
+
+
+ var price="10.9992323";
+ var age="2.0002";
+ console.log(parseInt(price)); // Convers floating number to integer number;
+ console.log(parseFloat(age),age); // Convers string number to float number;
+ console.log(parseFloat(price).toFixed(2)); //  displays the decimal points passed;
+
+
+
---
+++ 01Variables.js
@@ -0,0 +1,35 @@
+var fname="Vijay";
+var lname="Katakam";
+var price=10.33;
+
+
+console.log(typeof fname, typeof price);
+
+/***
+ * Data types in Javascript
+ */
+
+ // String
+ // Number
+ // Boolean
+ // undefined
+ // null
+
+
+ var salary;
+ console.log(salary);
+ var age=null;
+ console.log(age);
+
+ 
+
+ /**
+  * 
+  Question : 1 
+  What is the diffrence between null and undefined
+  Ans : If we declare a variable and assign nothing to it then ,the javascript by default assigns undefined.
+  where as null is a  data type assigned to a variable and indicates absence of data.
+  */
+
+
+
